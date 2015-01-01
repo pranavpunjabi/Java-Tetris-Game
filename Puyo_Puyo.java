0725846f160859374db3cd784fcbc79089a5b04c
pranavpunjabi <pranavpunjabi@gmail.com>
2015-01-01 00:10:42 -0500

    Added a Node() class in Puyo_Puyo.java to maintain a linked list kind of structure for pieces to store their positions

--- Puyo_Puyo.java
+++ Puyo_Puyo.java
@@ -741,3 +741,51 @@ class GamePane extends JComponent implements ActionListener
 }
 
 
+class Node
+{
+	//linked list is bulilt for the tetris of puyos to store their positions
+	int x,y;
+	Node nextnode;
+	Node prevnode;
+	public Node(int x,int y)
+	{
+		this.x=x;
+		this.y=y;
+		nextnode=null;
+		prevnode=null;
+	}
+	public Node(Node p)
+	{
+		x=p.x;
+		y=p.y;
+		nextnode=null;
+		prevnode=null;
+	
+	}
+	public void setNext(Node lnode)
+	{
+		nextnode=lnode;
+	}
+	public Node getNext()
+	{
+		return nextnode;
+	}
+	public void setPrev(Node lnode)
+	{
+		prevnode=lnode;
+	}
+	public Node getPrev()
+	{
+		return prevnode;
+	}
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+}
+
+
